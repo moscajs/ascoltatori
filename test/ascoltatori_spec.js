9fe222a753299d4ce6a501baf2156633b21f95f9
Matteo Collina <matteo.collina@gmail.com>
2012-10-27 15:25:18 +0200

    Extracted common spec for an ascoltatore.

--- test/ascoltatori_spec.js
+++ test/ascoltatori_spec.js
@@ -1,84 +1,14 @@
 
 var ascoltatori = require("../index");
 var expect = require("chai").expect;
-
-function wrap(done) {
-  return function() {
-    done();
-  };
-}
+var behave_like_an_ascoltatore = require("./behave_like_an_ascoltatore");
 
 describe(ascoltatori, function() {
 
+  behave_like_an_ascoltatore();
+
   beforeEach(function() {
     ascoltatori.reset();
-  });
-
-  it("should have have an on function", function() {
-    expect(ascoltatori).to.respondTo("on");
-  });
-
-  it("should have have an emit function", function() {
-    expect(ascoltatori).to.respondTo("emit");
-  });
-
-  it("should support 'on/emit' combination for pub/sub", function(done) {
-    ascoltatori.on("hello", wrap(done));
-    ascoltatori.emit("hello");
-  });
-
-  it("should support 'pub/sub combination for pub/sub", function(done) {
-    ascoltatori.sub("hello", wrap(done));
-    ascoltatori.pub("hello");
-  });
-
-  it("should support 'publish/subscribe combination for pub/sub", function(done) {
-    ascoltatori.subscribe("hello", wrap(done));
-    ascoltatori.publish("hello");
-  });
-
-  it("should support wildcards", function(done) {
-    ascoltatori.on("hello/*", wrap(done));
-    ascoltatori.emit("hello/42");
-  });
-
-  it("should publish the topic name", function(done) {
-    ascoltatori.on("hello/*", function(topic) {
-      expect(topic).to.equal("hello/42");
-      done();
-    });
-    ascoltatori.emit("hello/42");
-  });
-
-  it("should publish the passed argument", function(done) {
-    ascoltatori.on("hello/*", function(topic, value) {
-      expect(value).to.equal(42);
-      done();
-    });
-    ascoltatori.emit("hello/123", 42);
-  });
-
-  it("should have have a removeListener function", function() {
-    expect(ascoltatori).to.respondTo("removeListener");
-  });
-
-  it("should remove a listener", function(done) {
-    funcToRemove = function(topic, value) {
-      throw "this should never run";
-    };
-    ascoltatori.on("hello", funcToRemove);
-    ascoltatori.removeListener("hello", funcToRemove);
-    ascoltatori.on("hello", wrap(done));
-    ascoltatori.emit("hello");
-  });
-
-  it("should remove a listener for global searches", function(done) {
-    funcToRemove = function(topic, value) {
-      throw "this should never run";
-    };
-    ascoltatori.on("hello/*", funcToRemove);
-    ascoltatori.removeListener("hello/*", funcToRemove);
-    ascoltatori.on("hello/42", wrap(done));
-    ascoltatori.emit("hello/42");
+    this.instance = ascoltatori;
   });
 });
---
+++ test/behave_like_an_ascoltatore.js
@@ -0,0 +1,79 @@
+
+var expect = require("chai").expect;
+
+function wrap(done) {
+  return function() {
+    done();
+  };
+}
+
+module.exports = function() {
+
+  it("should have have an on function", function() {
+    expect(this.instance).to.respondTo("on");
+  });
+
+  it("should have have an emit function", function() {
+    expect(this.instance).to.respondTo("emit");
+  });
+
+  it("should support 'on/emit' combination for pub/sub", function(done) {
+    this.instance.on("hello", wrap(done));
+    this.instance.emit("hello");
+  });
+
+  it("should support 'pub/sub combination for pub/sub", function(done) {
+    this.instance.sub("hello", wrap(done));
+    this.instance.pub("hello");
+  });
+
+  it("should support 'publish/subscribe combination for pub/sub", function(done) {
+    this.instance.subscribe("hello", wrap(done));
+    this.instance.publish("hello");
+  });
+
+  it("should support wildcards", function(done) {
+    this.instance.on("hello/*", wrap(done));
+    this.instance.emit("hello/42");
+  });
+
+  it("should publish the topic name", function(done) {
+    this.instance.on("hello/*", function(topic) {
+      expect(topic).to.equal("hello/42");
+      done();
+    });
+    this.instance.emit("hello/42");
+  });
+
+  it("should publish the passed argument", function(done) {
+    this.instance.on("hello/*", function(topic, value) {
+      expect(value).to.equal(42);
+      done();
+    });
+    this.instance.emit("hello/123", 42);
+  });
+
+  it("should have have a removeListener function", function() {
+    expect(this.instance).to.respondTo("removeListener");
+  });
+
+  it("should remove a listener", function(done) {
+    funcToRemove = function(topic, value) {
+      throw "this should never run";
+    };
+    this.instance.on("hello", funcToRemove);
+    this.instance.removeListener("hello", funcToRemove);
+    this.instance.on("hello", wrap(done));
+    this.instance.emit("hello");
+  });
+
+  it("should remove a listener for global searches", function(done) {
+    funcToRemove = function(topic, value) {
+      throw "this should never run";
+    };
+    this.instance.on("hello/*", funcToRemove);
+    this.instance.removeListener("hello/*", funcToRemove);
+    this.instance.on("hello/42", wrap(done));
+    this.instance.emit("hello/42");
+  });
+};
