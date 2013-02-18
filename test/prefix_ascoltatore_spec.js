68e517dff0365b524921aed91ab456c8afd433aa
Matteo Collina <matteo.collina@gmail.com>
2013-02-18 13:33:45 +0000

    Added PrefixAscolatore
    PrefixAscoltatore is a decorator to publish messages on an ascoltatore
    with a prefix.
    
    Closes #21.

---
+++ test/prefix_ascoltatore_spec.js
@@ -0,0 +1,18 @@
+
+describe(ascoltatori.PrefixAscoltatore, function() {
+
+  behaveLikeAnAscoltatore();
+
+  beforeEach(function(done) {
+    this.included = new ascoltatori.MemoryAscoltatore();
+    this.instance = new ascoltatori.PrefixAscoltatore("/myprefix", this.included);
+    this.instance.on("ready", done);
+  });
+
+  it("should publish messages on the parent with a prefix", function(done) {
+    var that = this;
+    this.included.subscribe("/myprefix/hello", wrap(done), function() {
+      that.instance.publish("/hello", "world");
+    });
+  });
+});
