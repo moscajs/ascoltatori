31d84a64e957ca9dd78272e68fe0d984e8335c04
David Halls <dahalls@gmail.com>
2013-05-16 09:03:37 +0100

    Allow options.type to be a function (constructor) when building. Expose AbstractAscoltatore so such a function can inherit from it and pass ascoltatori module exports so there is no requirement on e.g. a mosca app to import ascoltatori. Test to check we can get an AbstractAscoltatore instance by specfying the constructor when building.

--- test/ascoltatori_spec.js
+++ test/ascoltatori_spec.js
@@ -110,6 +110,24 @@ describe("ascoltatori", function() {
       expect(a).to.be.instanceOf(ascoltatori.MemoryAscoltatore);
     });
 
+    it("should create a new FunctionAscoltatore", function() {
+      var a = ascoltatori.build({
+        json: false,
+        type: function (options, ascoltatori) {
+          this.__proto__ = ascoltatori.AbstractAscoltatore.prototype;
+          ascoltatori.AbstractAscoltatore.call(this);
+
+          this.emit("ready");
+
+          this.close = function (done) {
+            done();
+          }
+        }
+      });
+      toClose.push(a);
+      expect(a).to.be.instanceOf(ascoltatori.AbstractAscoltatore);
+    });
+
     it("should wrap it with a prefix", function(done) {
       var settings = redisSettings();
       settings.type = "redis";
