b112ce79f751ad863f014122e44f446b82ff625e
David Halls <dahalls@gmail.com>
2013-07-04 23:15:06 +0100

    Revert "Use async.series in DecoratorAscoltatore so can be sure publish/subscribe/unsubscribe is called"
    This reverts commit 245d0a61774fe556cab29ffe658303be0d0aa259.

--- test/ascoltatori_spec.js
+++ test/ascoltatori_spec.js
@@ -7,14 +7,6 @@ describe("ascoltatori", function() {
     return r;
   };
 
-  beforeEach(function() {
-    this.sandbox = sinon.sandbox.create();
-  });
-
-  afterEach(function() {
-    this.sandbox.restore();
-  });
-
   it("should delegate to the use ascoltatore for 'pub'", function() {
     var ascoltatore = makeFakeAscoltatore();
     var spy = this.sandbox.spy(ascoltatore, "publish");
