04553e2e084da1c8c58a86039b16b7c2b8bdacad
Matteo Collina <matteo.collina@gmail.com>
2013-10-31 14:15:10 +0100

    Fixed MongoDB binary problem.

--- test/mongo_ascoltatore_spec.js
+++ test/mongo_ascoltatore_spec.js
@@ -10,4 +10,14 @@ describe("ascoltatori.MongoAscoltatore", function() {
   afterEach(function(done) {
     this.instance.close(done);
   });
+
+  it("should publish a binary payload", function(done) {
+    var that = this;
+    that.instance.sub("hello/*", function(topic, value) {
+      expect(value).to.eql(new Buffer("42"));
+      done();
+    }, function() {
+      that.instance.pub("hello/123", new Buffer("42"));
+    });
+  });
 });
