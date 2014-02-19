369b369f3196325ec777c44f5123f278e7e9cbb3
Matteo Collina <matteo.collina@gmail.com>
2014-02-19 12:17:25 +0100

    Fixed Ascoltatori name in doc publishing script.

--- publish_docs.sh
+++ publish_docs.sh
@@ -2,8 +2,8 @@
 
 set -e
 rm -rf docs
-./node_modules/.bin/dox-foundation --source lib --target docs --title Mosca
-git stash	
+./node_modules/.bin/dox-foundation --source lib --target docs --title Ascoltatori
+git stash
 rm -rf /tmp/mosca-docs
 cp -R docs /tmp/mosca-docs
 git checkout gh-pages
